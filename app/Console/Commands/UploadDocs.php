400f8dbe696a7cafdac4c921a80acd19fd3c6428
daviddeveloper001 <deivydeveloper@gmail.com>
2025-01-04 19:33:55 -0500

    add CreateAssistant, PrepareDocs, UploadDocs

---
+++ app/Console/Commands/UploadDocs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UploadDocs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:upload-docs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
